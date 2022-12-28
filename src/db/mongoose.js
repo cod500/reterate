3c4455a438aea1e69c36ecf8a1d843465312ea50
Clay <cod5000@gmail.com>
2022-12-27 20:03:24 -0500

    MongoDB update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
+// mongodb://127.0.0.1:27017/rating-api
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
--- src/emails/account.js
+++ src/emails/account.js
@@ -3,14 +3,14 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const resetPasswordEmail = (email, token ) =>{
+const resetPasswordEmail = (email, token) => {
     sgMail.send({
         to: email,
         from: 'cod5000@gmail.com',
         subject: 'Restaurate Password Reset',
-        text: 'You have requested for password reset. \n\n'+
-        'Please click on the link to complete the process: \n\n'+
-        'http://localhost:3000/reset/'+token+'\n\n'
+        text: 'You have requested for password reset. \n\n' +
+            'Please click on the link to complete the process: \n\n' +
+            'http://localhost:3000/reset/' + token + '\n\n'
     });
 }
 
